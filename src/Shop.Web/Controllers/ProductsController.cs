c11d2e2ea3506ad54c78d826727b2ceb7af9c0f6
Dielektryk <dariusz94.putek@gmail.com>
2018-01-13 12:53:44 +0100

    Product list, commit 3

---
+++ src/Shop.Web/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Core.Domain;
+
+namespace Shop.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private static readonly List<Product> _products
+            = new List<Product>
+            {
+                new Product("Laptop","Electronics",3000),
+                new Product("Jeans","Trousers",150),
+                new Product("Hammer", "Tools",47),
+                new Product("Keyboard","Electronics",100)
+            };
+
+        public IActionResult Index()
+        {
+            return View(_products);
+        }
+    }
+}
