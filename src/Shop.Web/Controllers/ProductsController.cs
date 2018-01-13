a0b467de528d93aa1388100784541cb83a96c255
Dielektryk <dariusz94.putek@gmail.com>
2018-01-13 14:59:58 +0100

    commit 4

--- src/Shop.Web/Controllers/ProductsController.cs
+++ src/Shop.Web/Controllers/ProductsController.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Shop.Core.Domain;
+using Shop.Web.Models;
 
 namespace Shop.Web.Controllers
 {
+    [Route("products")]
     public class ProductsController : Controller
     {
         private static readonly List<Product> _products
@@ -15,9 +17,30 @@ namespace Shop.Web.Controllers
                 new Product("Keyboard","Electronics",100)
             };
 
+        [HttpGet]
         public IActionResult Index()
         {
             return View(_products);
+        }
+
+        [HttpGet("add")]
+        public IActionResult Add()
+        {
+            var viewModel = new ProductViewModel();
+
+            return View(viewModel);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(ProductViewModel viewModel)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+            _products.Add(new Product(viewModel.Name, viewModel.Category, viewModel.Price));
+
+            return RedirectToAction("Index");
         }
     }
 }
